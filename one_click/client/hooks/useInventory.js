ccc172a13c3bf6dfa26ecfdc091ed110b1a51b94
TimeBones <bendpenner@gmail.com>
2021-11-01 10:15:52 -0500

    Error handling and message

---
+++ one_click/client/hooks/useInventory.js
@@ -0,0 +1,27 @@
+import { useCallback } from "react";
+import { useHistory } from 'react-router';
+
+
+const useInventory = () => {
+    const history = useHistory();
+
+    const checkInventory = useCallback(async (lineItems) => {        
+        const lineIds = lineItems.map((item) => {
+          return item.product_data.product_id;
+        })
+        
+        const response = await fetch(`https://bold-benp.bold.ninja/inventory?ids=${lineIds.join(',')}`);
+        const inventory = await response.json();
+
+        lineItems.map((item) => {
+            const product = inventory.find(i => i.id === item.product_data.product_id);
+            if (product.stock < item.product_data.quantity) {
+                history.push('/inventory', inventory );
+            }
+        })
+    });
+  
+    return checkInventory;
+  };
+  
+  export default useInventory;
