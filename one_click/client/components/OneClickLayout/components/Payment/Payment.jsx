300b179a9b176d89b79a60d34e2b17be390c6564
arvinder-bold <arvinder.singh@boldcommerce.com>
2021-11-03 10:13:25 -0700

    HDLS-122 One Click - Payment Component
    **Changes**
    * Added payment component
    * Added discount code component

--- one_click/client/components/OneClickLayout/components/Payment/Payment.jsx
+++ one_click/client/components/OneClickLayout/components/Payment/Payment.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import EmptyState from '../EmptyState/EmptyState';
 import { BackButton } from '../BackButton';
 import { usePaymentMethod } from '@boldcommerce/checkout-react-components';
+import { BillingAddress } from '../BillingAddress';
+import { Discount } from '../Discount';
 import PaymentIframe from './PaymentIframe';
 import './Payment.css';
 
@@ -10,11 +12,9 @@ const PaymentMethod = ({ showPaymentMethod }) => {
   if (showPaymentMethod) {
     return (
       <div className="Payment">
+        <h1 className="Section__Title">Payment</h1>
         <BackButton />
         <section className="FieldSet FieldSet--PaymentMethod">
-          <div className="FieldSet__Header">
-            <div className="FieldSet__Heading">Payment methods</div>
-          </div>
           <div className="FieldSet__Content">
             <EmptyState title="To view payment options, complete filling in your address" />
             <PaymentIframe key="paymentIframe" hide />
@@ -25,14 +25,21 @@ const PaymentMethod = ({ showPaymentMethod }) => {
   }
 
   return (
-    <section className="FieldSet FieldSet--PaymentMethod">
-      <div className="FieldSet__Header">
-        <div className="FieldSet__Heading">Payment methods</div>
-      </div>
-      <div className="FieldSet__Content">
-        <PaymentIframe key="paymentIframe" />
-      </div>
-    </section>
+    <div className="Payment">
+      <h1 className="Section__Title">Payment</h1>
+      <BackButton />
+      <section className="FieldSet FieldSet--PaymentMethod">
+        <div className="FieldSet__Content">
+          <PaymentIframe key="paymentIframe" />
+        </div>
+        <div className="FieldSet__Content">
+          <Discount />
+        </div>
+        <div className="FieldSet__Content">
+          <BillingAddress />
+        </div>
+      </section>
+    </div>
   );
 };
 
