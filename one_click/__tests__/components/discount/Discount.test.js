2ae9b0dcaafdb1d5fe4d70d7f812b5a8c5f8326e
TimeBones <bendpenner@gmail.com>
2021-11-16 15:26:39 -0600

    BillingAddress and Discount snapshots

---
+++ one_click/__tests__/components/discount/Discount.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { render, fireEvent } from '@testing-library/react';
+import { Discount } from '../../../client/components/OneClickLayout/components/Discount/Discount';
+
+describe('Discount', () => {
+    test('renders Discount component closed', () => {
+        const { asFragment } = render(<Discount />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    test('renders Discount component opened', () => {
+        
+        const { container, asFragment } = render(
+            <Discount
+                discountCode="ExampleDiscount"
+            /> );
+        fireEvent.click(container.firstChild)
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    test('renders Discount component opened', () => {
+        const discountError = { discounts: "Invalid example discount" };
+        const { container, asFragment } = render(
+            <Discount
+                discountCode="ExampleDiscount"
+                discountErrors={discountError}
+            /> );
+        fireEvent.click(container.firstChild)
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
