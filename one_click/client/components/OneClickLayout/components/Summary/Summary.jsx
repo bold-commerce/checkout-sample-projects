a83b8732af7caf54e7e1b7527f86484891b3701a
TimeBones <bendpenner@gmail.com>
2021-11-26 17:09:52 -0600

    Mobile Animations and Styling

--- one_click/client/components/OneClickLayout/components/Summary/Summary.jsx
+++ one_click/client/components/OneClickLayout/components/Summary/Summary.jsx
@@ -8,6 +8,8 @@ import { Header } from '../Header';
 import classNames from 'classnames';
 import { CheckoutButton } from '../CheckoutButton';
 import './Summary.scss';
+import classNames from 'classnames';
+import { CheckoutButton } from '../CheckoutButton';
 
 const Summary = ({ show, onBack }) => {
     const { data: shipping } = useShippingLines();
