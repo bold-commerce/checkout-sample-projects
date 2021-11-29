554fc3baa00bfb929a670812d0482e0ee97e4dfd
kfalkowski11 <kfalkowski11@gmail.com>
2021-11-29 11:28:12 -0600

    HDLS-185: Add header to checkout component

---
+++ one_click/client/components/OneClickLayout/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Times } from "../Icons";
+import './Header.css';
+import { AppContext } from '../../context/AppContext';
+
+const Header = ({ title }) => {
+
+  const { setShowCheckout } = useContext(AppContext);
+
+  return (
+    <div className="Header">
+      <h1 className="Header__Title">{title}</h1>
+      <button className="Header__Times" onClick={() => setShowCheckout(false)}>
+        <Times />
+      </button>
+    </div>
+  )
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
+
+export default Header;
--- one_click/client/components/OneClickLayout/components/Icons/index.js
+++ one_click/client/components/OneClickLayout/components/Icons/index.js
@@ -1,3 +1,4 @@
 export { default as TimesCircle } from './TimesCirlce'
 export { default as ArrowRight } from './ArrowRight'
 export { default as CaretRight } from './CaretRight'
+export { default as Times } from './Times'
