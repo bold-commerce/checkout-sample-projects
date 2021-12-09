f3bda46d3e017421a59aba3affc945052e7282b2
Kara Falkowski <kfalkowski11@gmail.com>
2021-12-09 14:06:24 -0600

    remove unused imports

--- one_click/client/components/OneClickLayout/components/Index/IndexGuest.jsx
+++ one_click/client/components/OneClickLayout/components/Index/IndexGuest.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Button } from "@boldcommerce/stacks-ui";
+import { Button } from "@boldcommerce/stacks-ui/lib";
 import { useShippingAddress, useCountryInfo, useCheckoutStore, useCustomer } from "@boldcommerce/checkout-react-components";
 import { ShippingLines } from "../ShippingLines";
 import { AppContext } from "../../context/AppContext";
