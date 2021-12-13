4686d1db85a7530339e8f31ea2354eb7411c7c5e
TimeBones <bendpenner@gmail.com>
2021-12-13 09:26:49 -0600

    update billing classnames
    update to using BEM standard

--- one_click/client/components/OneClickLayout/components/Billing/Billing.jsx
+++ one_click/client/components/OneClickLayout/components/Billing/Billing.jsx
@@ -17,28 +17,28 @@ const Billing = ({section, onSectionChange}, ref) => {
     <div ref={ref} className={classNames('Sidebar Billing', section === 'billing' ? 'Sidebar--Show' : (section === 'summaryB' ? 'IndexPage--Hide' : 'Sidebar--Hide'))}>
       <Header title={websiteName} />
       
-      <div className="IndexGuest-summary">
+      <div className="IndexGuest__Summary">
         <button
-          className="IndexGuest-summary-btn"
+          className="IndexGuest__Summary__Btn"
           onClick={() => onSectionChange('summaryB')}
         >
-          <ChevronRight className="IndexGuest-chevron"/>
+          <ChevronRight className="IndexGuest__Chevron"/>
           <h2>Summary</h2>
           <Price amount={state.applicationState.order_total} />
       </button>
       </div>
 
-      <div className="Billing-payment">
-        <h2 className="IndexGuest-title">Payment method</h2>
+      <div className="Billing__Payment">
+        <h2 className="IndexGuest__Title">Payment method</h2>
         <PaymentMethod/>
       </div>
 
-      <div className="IndexGuest-footer">
-        <CheckoutButton className="CheckoutButton Billing-CheckoutButton" />
-        <div className="IndexGuest-footer-login">
+      <div className="IndexGuest-__Footer">
+        <CheckoutButton className="CheckoutButton Billing__Checkout__Btn" />
+        <div className="IndexGuest__Footer--login">
           <button className="link-btn" type="button" onClick={() => onSectionChange('/')}>Back to shipping</button>
         </div>          
-        <div className="IndexGuest-rights">{`All right reserved ${websiteName}`}</div>
+        <div className="IndexGuest__Rights">{`All right reserved ${websiteName}`}</div>
       </div>
     </div>
   )
