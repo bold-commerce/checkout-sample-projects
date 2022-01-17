1820e0246694cb0cb8720e7aac88c6ee4bef3154
Kara Falkowski <kfalkowski11@gmail.com>
2022-01-17 09:25:46 -0600

    open/close modal with events, style fixes for modal

--- one_click/src/components/Header/Header.jsx
+++ one_click/src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Times } from "../Icons";
 import './Header.css';
-import { AppContext } from '../../context/AppContext';
 
 const Header = ({ title }) => {
 
-  const { setShowCheckout } = useContext(AppContext);
+  const close = useCallback(() => {
+    const event = new CustomEvent("oneClick:close");
+    window.dispatchEvent(event);
+  }, []);
 
   return (
     <div className="Header">
       <h1 className="Header__Title">{title}</h1>
-      <button aria-label="Close" className="Header__Times" onClick={() => setShowCheckout(false)}>
+      <button aria-label="Close" className="Header__Times" onClick={close}>
         <Times />
       </button>
     </div>
--- one_click/src/index.js
+++ one_click/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import './i18n/config';
 import App from './App';
 
-window.addEventListener("openOneClickCheckout", () => {
+window.addEventListener("oneClick:open", () => {
   const div = document.createElement("div");
   div.setAttribute('id', 'checkout-app');
   document.body.appendChild(div);
 
   ReactDOM.render(<App />, document.getElementById('checkout-app'));
+});
+
+window.addEventListener("oneClick:close", () => {
+  const checkoutEl = document.getElementById('checkout-app')
+  ReactDOM.unmountComponentAtNode(checkoutEl);
+  checkoutEl.remove();
 });
