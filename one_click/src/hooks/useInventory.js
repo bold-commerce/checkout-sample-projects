538aefc1749070fc0adb98f0e631666f168eb6d7
Kara Falkowski <kfalkowski11@gmail.com>
2022-01-17 12:13:12 -0600

    update inventory hook for one click

--- one_click/src/hooks/useInventory.js
+++ one_click/src/hooks/useInventory.js
@@ -2,26 +2,43 @@ import { useCallback } from "react";
 
 
 const useInventory = () => {
-    const checkInventory = useCallback(async (lineItems) => {        
-        const lineIds = lineItems.map((item) => {
-          return item.product_data.product_id;
-        })
-        
-        const response = await fetch(`${process.env.INVENTORY_URL}?ids=${lineIds.join(',')}`);
-        const inventory = await response.json();
-        let inventoryIssues = null;
+  const checkInventory = useCallback(async (lineItems) => {        
+    let inventoryIssues = false;
+    let productInventory = {};
+    const variants = lineItems.map((lineItem) => lineItem.product_data.variant_id).join(',');
+    const response = await fetch(`${process.env.INVENTORY_URL}?variants=${variants}`);
+    let inventory = await response.json();
 
-        lineItems.forEach(item => {
-                
-            const product = inventory.find(i => i.id === item.product_data.product_id);
-            if ( product.tracking !== 'none' && product.stock < item.product_data.quantity) {                
-                inventoryIssues = inventory;        
-            }
-        });
-        return inventoryIssues;
+    lineItems.forEach((lineItem) => {
+      const index = lineItem.product_data.variant_id;
+      const product_id = inventory[index].product_id;
+
+      if (!(inventory[index].allow_backorder || inventory[index].inventory_tracker === 'none')) {
+        if (inventory[index].standard_tracking && inventory[index].quantity < lineItem.product_data.quantity) {
+          inventoryIssues = true;
+        } else if (!inventory[index].standard_tracking) {
+          // If there are multiple variants of the same product that has inventory tracked at product level
+          if (productInventory[product_id]) {
+            // If the product id already exists, set the inventory amount to it.
+            inventory[index].quantity = productInventory[inventory[index].product_id];
+          } else {
+            // If the product id does not exists, set the amount to the quantity.
+            productInventory[product_id] = inventory[index].quantity;
+          }
+
+          productInventory[product_id] = inventory[index].quantity - lineItem.product_data.quantity;
+          
+          if (productInventory[product_id] < 0) {
+            inventoryIssues = true;        
+          }
+        }
+      }
     });
-  
-    return checkInventory;
+    
+    return inventoryIssues ? inventory : null;
+  });
+
+  return checkInventory;
 };
   
 export default useInventory;
