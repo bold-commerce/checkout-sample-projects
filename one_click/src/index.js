5609ad9006412f03c86fc06b3cd550d791697e2f
Kara Falkowski <kfalkowski11@gmail.com>
2022-01-31 16:28:54 -0600

    close one click modal when page background clicked & prevent background scrolling

--- one_click/src/index.js
+++ one_click/src/index.js
@@ -3,16 +3,34 @@ import ReactDOM from "react-dom";
 import './i18n/config';
 import App from './App';
 
-window.addEventListener("oneClick:open", () => {
+const open = () => {
+  document.body.style.overflow = "hidden";
+
   const div = document.createElement("div");
   div.setAttribute('id', 'checkout-app');
   document.body.appendChild(div);
 
   ReactDOM.render(<App />, document.getElementById('checkout-app'));
-});
+}
 
-window.addEventListener("oneClick:close", () => {
+const close = () => {
   const checkoutEl = document.getElementById('checkout-app')
   ReactDOM.unmountComponentAtNode(checkoutEl);
   checkoutEl.remove();
+
+  document.body.style.overflow = "visible";
+}
+
+window.addEventListener("oneClick:open", () => {
+  open();
 });
+
+window.addEventListener("oneClick:close", () => {
+  close();
+});
+
+window.addEventListener("click", function(e){
+  if(e.target.className === 'OneClick--Modal'){
+    close();
+  }
+})
